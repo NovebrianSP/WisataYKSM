c00d85af1b35bd6b7c1ef32610bcab28bfeef4d5
Novebrian <notvebrian@gmail.com>
2025-06-19 18:28:50 +0700

    updating st.title

--- src/app.py
+++ src/app.py
@@ -33,7 +33,7 @@ st.set_page_config(page_title="Dashboard Destinasi Wisata Yogyakarta", layout="w
 page = st.sidebar.selectbox("Pilih Halaman", ["Dashboard", "Rekomendasi"])
 
 if page == "Dashboard":
-    st.title("WISATAKU")
+    st.title("<h1 style='text-align: center;'>WISATAKU</h1>", unsafe_allow_html=True,)
     st.header("Rekomendasi Destinasi Wisata Yogyakarta dan Semarang")
 
     # Filter di halaman utama
